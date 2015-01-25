17139dd6ea999d8d5b9ef09f2801475abb8a1e2f
uddhab <uddhabkshrestha@gmail.com>
2015-01-25 21:02:01 +0545

    added script and tests

---
+++ Tests/HeadCounterTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 1/25/2015
+ * Time: 8:54 PM
+ */
+
+class HeadCounterTest extends PHPUnit_Framework_TestCase
+{
+    public function testTrigger()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
---
+++ bin/buildscript.bat
@@ -0,0 +1,3 @@
+php "C:\Users\User\Downloads\projects\taskmanager\composer.phar" install
+php "C:\Users\User\Downloads\projects\taskmanager\php-cs-fixer.phar" fix .
+vendor\bin\phpunit.bat Tests
